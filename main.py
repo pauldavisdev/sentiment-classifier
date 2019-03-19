f6a40a48304ea96aba2673de12ebc70b24d42505
pauldavisdev <pauldavis4@gmail.com>
2019-03-19 21:58:06 +0000

    Refactor

--- main.py
+++ main.py
@@ -5,94 +5,60 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer, one_hot
 from neural_network import NeuralNetwork
 from preprocessor import Preprocessor
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import time, os
 import pandas as pd
 import pickle
 
-def main():
-    num_csv_rows = 200000
+def prepare_data():
+    num_csv_rows = 30000
 
-    max_len = 100
+    max_len = 140
 
-    vocab_size = 20000
+    train, test = read_csv(num_csv_rows)
 
-    # train, test = read_csv(num_csv_rows)
+    testX, testY = test['tweet'].values, test['polarity'].values
+    trainX, trainY = train['tweet'].values, train['polarity'].values
 
-    # testX, testY = test['tweet'].values, test['polarity'].values
-    # trainX, trainY = train['tweet'].values, train['polarity'].values
+    trainY = np.where(trainY == 4, 1, trainY)
+    testY = np.where(testY == 4, 1, testY)
 
-    # trainY = np.where(trainY == 4, 1, trainY)
-    # testY = np.where(testY == 4, 1, testY)
+    preprocessor = Preprocessor()
 
-    # preprocessor = Preprocessor()
+    trainX, testX = preprocessor.clean_texts(trainX, testX)
 
-    # trainX, testX = preprocessor.clean_texts(trainX, testX)
+    tokenizer = Tokenizer()
 
-    # tokenizer = Tokenizer()
-
-    # tokenizer.fit_on_texts(trainX)
-
-    # tokenizer.num_words = vocab_size
+    tokenizer.fit_on_texts(trainX)
     
-    # encoded_train = []
+    vocab_size = int(len(tokenizer.word_index) * 0.90) + 1
 
-    # for tweet in trainX:
-    #     encoded_train.append(one_hot(tweet, vocab_size))
+    tokenizer.num_words = vocab_size
+
+    encoded_train = []
+
+    for tweet in trainX:
+        encoded_train.append(one_hot(tweet, vocab_size))
     
-    # #wait = input("PRESS ENTER TO CONTINUE.")
+    trainX = pad_sequences(encoded_train, maxlen=max_len, padding='post')
 
-    # trainX = pad_sequences(encoded_train, maxlen=max_len, padding='post')
+    tokenizer.fit_on_texts(testX)
 
-    # tokenizer.fit_on_texts(testX)
+    encoded_test = []
 
-    # encoded_test = []
+    for tweet in testX:
+        encoded_test.append(one_hot(tweet, vocab_size))
 
-    # for tweet in testX:
-    #     encoded_test.append(one_hot(tweet, vocab_size))
+    testX = pad_sequences(encoded_test, maxlen=max_len, padding='post')
 
-    # testX = pad_sequences(encoded_test, maxlen=max_len, padding='post')
+    return trainX, trainY, testX, testY, vocab_size
 
-
-    file = open('test.pkl', 'rb')
-    trainX = pickle.load(file)
-    trainY = pickle.load(file)
-    testX = pickle.load(file)
-    testY = pickle.load(file)
-    file.close()
-
-    neural_network = NeuralNetwork()
-
-    neural_network.generate_model(vocab_size)
-
-    neural_network.model.summary()
-
-    neural_network.model.compile(optimizer=tf.train.AdamOptimizer(),
-            loss='binary_crossentropy',
-            metrics=['accuracy'])
-
-    # file = open('test.pkl','wb')
-    # pickle.dump(trainX, file)
-    # pickle.dump(trainY, file)
-    # pickle.dump(testX, file)
-    # pickle.dump(testY, file)
-    # file.close()
-    
-    x_val = trainX[:5000]
-    partial_x_train = trainX[5000:]
-
-    y_val = trainY[:5000]
-    partial_y_train = trainY[5000:]
-
-    history = neural_network.fit_model(partial_x_train, partial_y_train, 
-                                        x_val, y_val)
-
-    results = neural_network.model.evaluate(testX, testY)
-
-    print(neural_network.model.metrics_names)
-    print(results)
+def plot_graph(history):
 
     history_dict = history.history
+
     history_dict.keys()
 
     acc = history.history['acc']
@@ -126,6 +92,9 @@ def main():
 
     plt.show()
 
+def create_csv_dataframe(history):
+    history_dict = history.history
+
     df = pd.DataFrame.from_dict(history_dict)
     df['test_loss'] = results[0]
     df['test_acc'] = results[1]
@@ -137,7 +106,57 @@ def main():
     df['patience'] = neural_network.patience
     df = df.round(decimals=4)
 
-    write_csv(df)
+    return df
+
+
+def main():
+    
+    trainX, trainY, testX, testY, vocab_size = prepare_data()
+
+    # file = open('data.pkl', 'rb')
+    # trainX = pickle.load(file)
+    # trainY = pickle.load(file)
+    # testX = pickle.load(file)
+    # testY = pickle.load(file)
+    # vocab_size = pickle.load(file)
+    # file.close()
+
+    # file = open('data.pkl','wb')
+    # pickle.dump(trainX, file)
+    # pickle.dump(trainY, file)
+    # pickle.dump(testX, file)
+    # pickle.dump(testY, file)
+    # pickle.dump(vocab_size, file)
+    # file.close()
+
+    neural_network = NeuralNetwork()
+
+    neural_network.generate_model(vocab_size)
+
+    neural_network.model.summary()
+    
+    neural_network.model.compile(optimizer=tf.train.AdamOptimizer(),
+            loss='binary_crossentropy',
+            metrics=['accuracy'])
+   
+    x_val = trainX[:10000]
+    partial_x_train = trainX[10000:]
+
+    y_val = trainY[:10000]
+    partial_y_train = trainY[10000:]
+
+    history = neural_network.fit_model(partial_x_train, partial_y_train, 
+                                        x_val, y_val)
+
+    results = neural_network.model.evaluate(testX, testY)
+
+    print(neural_network.model.metrics_names)
+
+    print(results)
+
+    plot_graph(history)
+
+    write_csv(create_csv_dataframe(history))
 
 if __name__ == '__main__':
     main()
