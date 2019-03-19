783cc3db20070163432d11c029233b05f39d2287
Paul Davis <pauldavis4@gmail.com>
2019-03-19 00:07:11 +0000

    Classifier runs and displays graph of results, but lacks preprocessing

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from csv_handler import read_csv
 import numpy as np
+import tensorflow as tf
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from neural_network import NeuralNetwork
+import matplotlib.pyplot as plt
 
 def main():
-    num_csv_rows = 10000
+    num_csv_rows = 100000
+
+    max_len = 140
 
     train, test = read_csv(num_csv_rows)
 
@@ -11,6 +18,80 @@ def main():
 
     trainY = np.where(trainY == 4, 1, trainY)
     testY = np.where(testY == 4, 1, testY)
+
+    tokenizer = Tokenizer()
+
+    tokenizer.fit_on_texts(trainX)
+
+    encoded_texts = tokenizer.texts_to_sequences(trainX)
+
+    trainX = pad_sequences(encoded_texts, maxlen=max_len, padding='post')
+
+    tokenizer.fit_on_texts(testX)
+
+    encoded_texts = tokenizer.texts_to_sequences(testX)
+
+    testX = pad_sequences(encoded_texts, maxlen=max_len, padding='post')
+
+    vocab_size = len(tokenizer.word_index) + 1
+
+    neural_network = NeuralNetwork()
+
+    neural_network.generate_model(vocab_size)
+
+    neural_network.model.summary()
+
+    neural_network.model.compile(optimizer=tf.train.AdamOptimizer(),
+            loss='binary_crossentropy',
+            metrics=['accuracy'])
+
+    x_val = trainX[:5000]
+    partial_x_train = trainX[5000:]
+
+    y_val = trainY[:5000]
+    partial_y_train = trainY[5000:]
+
+    history = neural_network.fit_model(partial_x_train, partial_y_train, 
+                                        x_val, y_val)
+
+    results = neural_network.model.evaluate(testX, testY)
+
+    print(neural_network.model.metrics_names)
+    print(results)
+
+    history_dict = history.history
+    history_dict.keys()
+
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    # "bo" is for "blue dot"
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    # b is for "solid blue line"
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.show()
+
+    plt.clf()   # clear figure
+    acc_values = history_dict['acc']
+    val_acc_values = history_dict['val_acc']
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.show()
 
 if __name__ == '__main__':
     main()
